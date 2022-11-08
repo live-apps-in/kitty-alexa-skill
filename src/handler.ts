34c8d003af5ee1265721b33540ee5062e5c1b440
jaga-live <jagadheesh6@gmail.com>
2022-11-08 17:18:31 +0530

    feat - greet intent

--- src/handler.ts
+++ src/handler.ts
@@ -1,118 +1,100 @@
-import {
-    ErrorHandler,
-    HandlerInput,
-    RequestHandler,
-    SkillBuilders,
-} from 'ask-sdk-core';
-import {
-    Response,
-    SessionEndedRequest,
-} from 'ask-sdk-model';
+import { ErrorHandler, HandlerInput, RequestHandler, SkillBuilders } from 'ask-sdk-core';
+import { Response, SessionEndedRequest } from 'ask-sdk-model';
+
+import * as Alexa from 'ask-sdk';
+
+///Intents
+import { GreetIntentHandler } from './intent/GreetIntent';
 
 const LaunchRequestHandler: RequestHandler = {
-    canHandle(handlerInput: HandlerInput): boolean {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'LaunchRequest';
-    },
-    handle(handlerInput: HandlerInput): Response {
-        const speechText = 'Hello, kitty chan is ready to help you!';
+	canHandle(handlerInput: HandlerInput): boolean {
+		const request = handlerInput.requestEnvelope.request;
+		return request.type === 'LaunchRequest';
+	},
+	handle(handlerInput: HandlerInput): Response {
+		const speechText = 'Hello, kitty chan is ready to help you!';
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .withSimpleCard('Hello, kitty chan is ready to help you!', speechText)
-            .getResponse();
-    },
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.reprompt(speechText)
+			.withSimpleCard('Hello, kitty chan is ready to help you!', speechText)
+			.getResponse();
+	},
 };
 
-
-const HelloKittyIntentHandler: RequestHandler = {
-    canHandle(handlerInput: HandlerInput): boolean {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'IntentRequest'
-            && request.intent.name === 'HelloKittyIntent';
-    },
-    handle(handlerInput: HandlerInput): Response {
-        const speechText = 'kitty said hello';
-
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .withSimpleCard('kitty said hello', speechText)
-            .getResponse();
-    },
-};
 
 
 const HelpIntentHandler: RequestHandler = {
-    canHandle(handlerInput: HandlerInput): boolean {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'IntentRequest'
+	canHandle(handlerInput: HandlerInput): boolean {
+		const request = handlerInput.requestEnvelope.request;
+		return request.type === 'IntentRequest'
             && request.intent.name === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput: HandlerInput): Response {
-        const speechText = 'You can ask me the weather!';
+	},
+	handle(handlerInput: HandlerInput): Response {
+		const speechText = 'You can ask me the weather!';
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .reprompt(speechText)
-            .withSimpleCard('You can ask me the weather!', speechText)
-            .getResponse();
-    },
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.reprompt(speechText)
+			.withSimpleCard('You can ask me the weather!', speechText)
+			.getResponse();
+	},
 };
 
 const CancelAndStopIntentHandler: RequestHandler = {
-    canHandle(handlerInput: HandlerInput): boolean {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'IntentRequest'
+	canHandle(handlerInput: HandlerInput): boolean {
+		const request = handlerInput.requestEnvelope.request;
+		return request.type === 'IntentRequest'
             && (request.intent.name === 'AMAZON.CancelIntent'
                 || request.intent.name === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput: HandlerInput): Response {
-        const speechText = 'Goodbye!';
+	},
+	handle(handlerInput: HandlerInput): Response {
+		const speechText = 'Goodbye!';
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .withSimpleCard('Goodbye!', speechText)
-            .withShouldEndSession(true)
-            .getResponse();
-    },
+		return handlerInput.responseBuilder
+			.speak(speechText)
+			.withSimpleCard('Goodbye!', speechText)
+			.withShouldEndSession(true)
+			.getResponse();
+	},
 };
 
 
 const SessionEndedRequestHandler: RequestHandler = {
-    canHandle(handlerInput: HandlerInput): boolean {
-        const request = handlerInput.requestEnvelope.request;
-        return request.type === 'SessionEndedRequest';
-    },
-    handle(handlerInput: HandlerInput): Response {
-        console.log(`Session ended with reason: ${(handlerInput.requestEnvelope.request as SessionEndedRequest).reason}`);
+	canHandle(handlerInput: HandlerInput): boolean {
+		const request = handlerInput.requestEnvelope.request;
+		return request.type === 'SessionEndedRequest';
+	},
+	handle(handlerInput: HandlerInput): Response {
+		console.log(`Session ended with reason: ${(handlerInput.requestEnvelope.request as SessionEndedRequest).reason}`);
 
-        return handlerInput.responseBuilder.getResponse();
-    },
+		return handlerInput.responseBuilder.getResponse();
+	},
 };
 
 const ErrorHandler: ErrorHandler = {
-    canHandle(handlerInput: HandlerInput, error: Error): boolean {
-        return true;
-    },
-    handle(handlerInput: HandlerInput, error: Error): Response {
-        console.log(`Error handled: ${error.message}`);
+	canHandle(handlerInput: HandlerInput, error: Error): boolean {
+		return true;
+	},
+	handle(handlerInput: HandlerInput, error: Error): Response {
+		console.log(`Error handled: ${error.message}`);
 
-        return handlerInput.responseBuilder
-            .speak('Sorry, I don\'t understand your command. Please say it again.')
-            .reprompt('Sorry, I don\'t understand your command. Please say it again.')
-            .getResponse();
-    }
+		return handlerInput.responseBuilder
+			.speak('Sorry, I don\'t understand your command. Please say it again.')
+			.reprompt('Sorry, I don\'t understand your command. Please say it again.')
+			.getResponse();
+	}
 };
 
 
 module.exports.hello = SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloKittyIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-    )
-    .addErrorHandlers(ErrorHandler)
-    .lambda();
+	.addRequestHandlers(
+		LaunchRequestHandler,
+		GreetIntentHandler,
+		HelpIntentHandler,
+		CancelAndStopIntentHandler,
+		SessionEndedRequestHandler,
+	)
+	.addErrorHandlers(ErrorHandler)
+	.withApiClient(new Alexa.DefaultApiClient())
+	.lambda();
---
+++ src/intent/GreetIntent.ts
@@ -0,0 +1,28 @@
+import { HandlerInput, RequestHandler } from 'ask-sdk-core';
+import { Response } from 'ask-sdk-model';
+
+///Greet Users
+export const GreetIntentHandler: RequestHandler = {
+	canHandle(handlerInput: HandlerInput): boolean {
+		const request: any = handlerInput.requestEnvelope.request;
+		return request.type === 'IntentRequest'
+            && request.intent.name === 'GreetIntent';
+	},
+
+	async handle(handlerInput: HandlerInput): Promise<Response> {
+		const { serviceClientFactory } = handlerInput;
+
+		///fetch User info
+		const upsServiceClient = serviceClientFactory.getUpsServiceClient();
+		const profileName = await upsServiceClient.getProfileGivenName();
+
+		const speechResponse = `Hello, ${profileName}`;
+		console.log(profileName, '-------profile name-------');
+
+		return handlerInput.responseBuilder
+			.speak(speechResponse)
+			.reprompt('Nice to meet you!')
+			.withSimpleCard(speechResponse, speechResponse)
+			.getResponse();
+	},
+};
